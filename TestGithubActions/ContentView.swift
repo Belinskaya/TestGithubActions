5f1fb3eb08193720c1813b928232e4f5a1a7949a
Kate Belinskaya <kate@byte.co>
2020-05-25 09:43:08 -0400

    Initial Commit

---
+++ TestGithubActions/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestGithubActions
+//
+//  Created by Kate Belinskaya on 5/25/20.
+//  Copyright © 2020 Kate Belinskaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
